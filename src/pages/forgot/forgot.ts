e1ab6fd50044a48175d3f4119079119a4c56eabb
liuhaiqiang <liuhaiqiang@ajmide.com>
2018-10-08 17:52:09 +0800

    modify

--- src/pages/forgot/forgot.ts
+++ src/pages/forgot/forgot.ts
@@ -17,12 +17,13 @@ export class ForgotPage {
   public user: any = {
     tel: '',
     passwd: '',
-    passwd_type: ''
+    passwd_type: 'password',
+    pwd_name: 'eye-off'
   };
   public verifyCode: any = {
     tips: '获取验证码',
     count: 120,
-    disable: false,
+    disable: true,
     code: ''
   };
   public modifyStatus: boolean = true;
@@ -37,9 +38,13 @@ export class ForgotPage {
   onChange(index) {
     if (/^1[34578]\d{9}$/.test(this.user.tel)
       && this.user.passwd.length >= 6
-      && this.user.passwd.length <= 20
-      && this.verifyCode.code.length === 6) {
-      this.modifyStatus = false;
+      && this.user.passwd.length <= 20) {
+      if (this.verifyCode.disable && index !== 2) {
+        this.verifyCode.disable = false;
+      }
+      if (this.verifyCode.code.length === 6) {
+        this.modifyStatus = false;
+      }
     }
     else {
       this.modifyStatus = true;
@@ -54,16 +59,26 @@ export class ForgotPage {
   onClick(index) {
     switch (index) {
       case 0:
-        this.verifyCode.disable = true;
         if (/^1[34578]\d{9}$/.test(this.user.tel)
           && this.user.passwd.length >= 6
           && this.user.passwd.length <= 20) {
+          this.verifyCode.disable = true;
           this.get_code();
         }
         break;
       case 1:
         this.modify_passwd();
 
+        break;
+      case 2:
+        if (this.user.pwd_name === 'eye') {
+          this.user.pwd_name = 'eye-off';
+          this.user.passwd_type = 'password';
+        }
+        else {
+          this.user.pwd_name = 'eye';
+          this.user.passwd_type = 'text';
+        }
         break;
       default:
         break
@@ -93,13 +108,14 @@ export class ForgotPage {
   }
 
   modify_passwd() {
-    let modal = this.toastCtrl.create({
-      message: '修改密码成功，请重新登录',
-      duration: 2000,
-      position: 'top'
-
-    });
-    modal.present();
+    // console.log('modify_passwd');
+    // let modal = this.toastCtrl.create({
+    //   message: '修改密码成功，请重新登录',
+    //   duration: 2000,
+    //   position: 'top'
+    //
+    // });
+    // modal.present();
     this.navCtrl.push('LoginPage');
   }
 
